f395f7d7aca5c297304849247ba70a6fdf4f5844
ABHISHEK <abhidk62@gmail.com>
2025-03-26 01:28:11 +0530

    deploy-4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
 import { Toaster } from "react-hot-toast";
-import SignUp from "./pages/signup/signup";
+import SignUp from "./pages/signup/Signup";
 import { useAuthContext } from "./context/AuthContext";
 const App = () => {
   const { authUser } = useAuthContext();
