2d11e6bac84877d367883d2429192622c310fefa
ABHISHEK <abhidk62@gmail.com>
2025-03-26 01:36:22 +0530

    Fixed import path for useSignUp

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 import { useState } from "react";
-import useSignUp from "../../hooks/useSignUp";
+import useSignUp from "../../hooks/UseSignUp";
 
 const SignUp = () => {
   const [input, setInput] = useState({
